c6e367be716bc534927a6d29eb97da465f480e94
kimmyounha <121646831+kimmyounha@users.noreply.github.com>
2024-03-05 21:32:30 +0900

    [level 0] Title: 수 조작하기 1, Time: 3.33 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,15 @@
+function solution(n, control) {
+    for(var i = 0; i < control.length; i++){
+        const index = control[i];
+        if(index === "w")
+            n += 1;
+        if(index === "s")
+            n -= 1;
+        if(index === "d")
+            n += 10;
+        if(index === "a")
+            n -= 10;
+    }
+    var answer = n;
+    return answer;
+}
