3db3de93a08f21d458f1cbe286319e903270b221
kimmyounha <121646831+kimmyounha@users.noreply.github.com>
2024-03-07 20:16:21 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    for(let i = 1; i*i <= n; i++){
+        if(i*i==n){
+            answer = (i+1)*(i+1);
+        }else{ 
+            answer = -1;
+        }
+    }
+    return answer;
+}
